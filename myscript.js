f26e07c75c7e39aed63c8f7aea2ef1dc545364b5
Rajveer20 <72323161+Rajveer20@users.noreply.github.com>
2020-10-20 22:57:04 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,33 @@
+
+
+function mouseOver1() {
+  document.getElementById('twitter').src = "1x/Asset 5.png";
+}
+
+function mouseOut1() {
+  document.getElementById('twitter').src = "1x/Asset 1.png";
+}
+
+function mouseOver2() {
+  document.getElementById('behance').src = "1x/Asset 6.png";
+}
+
+function mouseOut2() {
+  document.getElementById('behance').src = "1x/Asset 2.png";
+}
+
+function mouseOver3() {
+  document.getElementById('instagram').src = "1x/Asset 7.png";
+}
+
+function mouseOut3() {
+  document.getElementById('instagram').src = "1x/Asset 3.png";
+}
+
+function mouseOver4() {
+  document.getElementById('whatsapp').src = "1x/Asset 8.png";
+}
+
+function mouseOut4() {
+  document.getElementById('whatsapp').src = "1x/Asset 4.png";
+}
